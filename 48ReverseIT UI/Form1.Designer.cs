e5a343965d3f87599eb61ca1d1ed2650f0dee0a1
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-24 13:32:02 +0000

    Palindrome Check Working

--- 48ReverseIT UI/Form1.Designer.cs
+++ 48ReverseIT UI/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.tbxInput = new System.Windows.Forms.TextBox();
             this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.lblPali = new System.Windows.Forms.Label();
+            this.lblPaliOut = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbxInput
             // 
             this.tbxInput.Location = new System.Drawing.Point(12, 12);
             this.tbxInput.Name = "tbxInput";
-            this.tbxInput.Size = new System.Drawing.Size(202, 20);
+            this.tbxInput.Size = new System.Drawing.Size(129, 20);
             this.tbxInput.TabIndex = 0;
             this.tbxInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.tbxInput.TextChanged += new System.EventHandler(this.tbxInput_TextChanged);
@@ -46,18 +48,41 @@
             this.tbxOutput.Location = new System.Drawing.Point(12, 38);
             this.tbxOutput.Name = "tbxOutput";
             this.tbxOutput.ReadOnly = true;
-            this.tbxOutput.Size = new System.Drawing.Size(202, 20);
+            this.tbxOutput.Size = new System.Drawing.Size(110, 20);
             this.tbxOutput.TabIndex = 1;
             this.tbxOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblPali
+            // 
+            this.lblPali.AutoSize = true;
+            this.lblPali.Location = new System.Drawing.Point(19, 70);
+            this.lblPali.Name = "lblPali";
+            this.lblPali.Size = new System.Drawing.Size(62, 13);
+            this.lblPali.TabIndex = 2;
+            this.lblPali.Text = "Palindrome:";
+            // 
+            // lblPaliOut
+            // 
+            this.lblPaliOut.AutoSize = true;
+            this.lblPaliOut.Location = new System.Drawing.Point(87, 70);
+            this.lblPaliOut.Name = "lblPaliOut";
+            this.lblPaliOut.Size = new System.Drawing.Size(32, 13);
+            this.lblPaliOut.TabIndex = 3;
+            this.lblPaliOut.Text = "False";
             // 
             // FrmReverse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(226, 260);
+            this.ClientSize = new System.Drawing.Size(153, 151);
+            this.Controls.Add(this.lblPaliOut);
+            this.Controls.Add(this.lblPali);
             this.Controls.Add(this.tbxOutput);
             this.Controls.Add(this.tbxInput);
+            this.MaximumSize = new System.Drawing.Size(169, 307);
+            this.MinimumSize = new System.Drawing.Size(169, 0);
             this.Name = "FrmReverse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "48 - Reverse IT";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,6 +93,8 @@
 
         private System.Windows.Forms.TextBox tbxInput;
         private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.Label lblPali;
+        private System.Windows.Forms.Label lblPaliOut;
     }
 }
 
--- 48ReverseIT UI/Form1.cs
+++ 48ReverseIT UI/Form1.cs
@@ -1,11 +1,11 @@
-﻿using System;using System.Collections.Generic;using System.ComponentModel;using System.Data;using System.Drawing;using System.Linq;using System.Text;using System.Threading.Tasks;using System.Windows.Forms;
-
+﻿using System;using System.Windows.Forms;
 namespace _48ReverseIT_UI {
     public partial class FrmReverse : Form {
         public FrmReverse() {
-            InitializeComponent(); }
+            InitializeComponent();  }
 
-
-        private void tbxInput_TextChanged(object sender, EventArgs e) { tbxOutput.Text = "";
+        public void tbxInput_TextChanged(object sender, EventArgs e) { tbxOutput.Text = "";
             for (int i = tbxInput.Text.Length - 1; i >= 0; i--) {
-                tbxOutput.Text += tbxInput.Text.Substring(i, 1); } } } }
+                tbxOutput.Text += tbxInput.Text.Substring(i, 1); } 
+            if (tbxInput.Text == tbxOutput.Text && tbxInput.Text != "") { lblPaliOut.Text = "True"; }
+            else { lblPaliOut.Text = "False"; } } } }
