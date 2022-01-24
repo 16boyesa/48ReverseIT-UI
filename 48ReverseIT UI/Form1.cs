e1e3848046085f96b8e0b5bf2df59865d106ffdc
Adam Boyes <16BoyesA@fulford.york.sch.uk>
2022-01-24 13:35:21 +0000

    Added Comments

--- 48ReverseIT UI/Form1.cs
+++ 48ReverseIT UI/Form1.cs
@@ -2,10 +2,10 @@
 namespace _48ReverseIT_UI {
     public partial class FrmReverse : Form {
         public FrmReverse() {
-            InitializeComponent();  }
+            InitializeComponent();  }                                                                       // Sets up the Form
 
-        public void tbxInput_TextChanged(object sender, EventArgs e) { tbxOutput.Text = "";
-            for (int i = tbxInput.Text.Length - 1; i >= 0; i--) {
-                tbxOutput.Text += tbxInput.Text.Substring(i, 1); } 
-            if (tbxInput.Text == tbxOutput.Text && tbxInput.Text != "") { lblPaliOut.Text = "True"; }
-            else { lblPaliOut.Text = "False"; } } } }
+        public void tbxInput_TextChanged(object sender, EventArgs e) { tbxOutput.Text = "";                 // Resets the Output Text and runs code whenever the Input changes
+            for (int i = tbxInput.Text.Length - 1; i >= 0; i--) {                                           // Loops through String
+                tbxOutput.Text += tbxInput.Text.Substring(i, 1); }                                          // Reverses String
+            if (tbxInput.Text == tbxOutput.Text && tbxInput.Text != "") { lblPaliOut.Text = "True"; }       // Checks for Palindromes
+            else { lblPaliOut.Text = "False"; } } } }                                                       // Resets Palindrome Check
